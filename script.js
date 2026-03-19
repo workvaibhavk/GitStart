3f01db11ed4dd3504708d64422d58dbb8981d1ad
Vaibhav Kamble <itsvaibhav.work@gmail.com>
2026-03-20 01:10:34 +0530

    finalised index page

--- script.js
+++ script.js
@@ -18,21 +18,40 @@ function showTab(tabName) {
     // console.log(currentTab);
     const currentBtn = document.getElementById(tabName + "Btn");
     // console.log(currentBtn);
-
+    const prevTab = document.querySelector(".active")
     const prevBtn = document.querySelector(".activeBtn")
     prevBtn.classList.remove("activeBtn");
+    prevTab.classList.remove("active");
+    prevTab.classList.remove("block");
+    prevTab.classList.add("hidden");
 
     currentBtn.classList.add("activeBtn")
-
     currentTab.classList.remove("hidden");
     currentTab.classList.add("block");
     currentTab.classList.add("active");
 
     // const prevTab = document.getElementByClassName("active");
-    const prevTab = document.querySelector(".active")
-    prevTab.classList.remove("active");
-    prevTab.classList.remove("block");
-    prevTab.classList.add("hidden");
 
 
 }
+
+
+
+
+
+
+
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute("href"));
+
+        target.scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    });
+});
